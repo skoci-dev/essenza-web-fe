cabaef87be33dc892149432db205c65063ea0393
febri <febri.triakbar@yahoo.com>
2025-11-06 16:48:56 +0700

    Fixing Front Page

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -1,41 +1,20 @@
 import HeroSection from '@/components/section/home/HeroSection'
 
-// import UsefulFeature from '@/components/section/home/UsefulFeature'
 import CustomerReviews from '@/components/section/home/CustomerReviews'
-import OurTeam from '@/components/section/home/OurTeam'
 import PricingPlan from '@/components/section/home/Pricing'
 import ProductStat from '@/components/section/home/ProductStat'
-import Faqs from '@/components/section/home/Faqs'
 import GetStarted from '@/components/section/home/GetStarted'
-import ContactUs from '@/components/section/home/ContactUs'
-
-import { getServerMode } from '@core/utils/serverHelpers'
-
-import { useSettings } from '@core/hooks/useSettings'
 
 const HomePage = () => {
   // Hooks
-  const { updatePageSettings } = useSettings()
-
-  // For Page specific settings
-  useEffect(() => {
-    return updatePageSettings({
-      skin: 'default'
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <>
       <HeroSection />
-      {/* <UsefulFeature /> */}
       <CustomerReviews />
-      {/* <OurTeam /> */}
       <PricingPlan />
       <ProductStat />
-      {/* <Faqs /> */}
       <GetStarted />
-      {/* <ContactUs /> */}
     </>
   )
 }
