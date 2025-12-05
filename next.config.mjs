a6e3b164c328eae972c54b2c1fb822fd9f6840bd
febri <febri.triakbar@yahoo.com>
2025-12-05 17:30:03 +0700

    Change Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
-
 const nextConfig = {
   basePath: process.env.BASEPATH,
-  redirects: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/id',
-        permanent: true,
-        locale: false
-      }
-    ]
-  },
+
   async rewrites() {
+    const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
+
+    if (!BACKEND_URL.startsWith('http')) {
+      console.error('Rewrite failed: NEXT_PUBLIC_API_URL must start with http:// or https://')
+    }
+
     return [
       {
         source: '/media/uploads/:path*',
-        destination: `${API_URL}/media/uploads/:path*`
+        destination: `${BACKEND_URL}/media/uploads/:path*`
       }
     ]
   }
